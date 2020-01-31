9ca58b2d27b1ef8b8f124caea95f6512f129c0e5
Jason Simeone <jason@prometheusresearch.com>
2020-01-31 14:05:29 -0500

    Moved to git

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ LONG_DESCRIPTION = open('README').read()
 AUTHOR = "Kirill Simonov (Prometheus Research, LLC)"
 AUTHOR_EMAIL = "xi@resolvent.net"
 LICENSE = "BSD"
-URL = "http://bitbucket.org/prometheus/sphinxcontrib-rextheme"
+URL = "https://github.com/prometheusresearch/sphinxcontrib-rextheme"
 DOWNLOAD_URL = "http://pypi.python.org/pypi/sphinxcontrib-rextheme"
 CLASSIFIERS = [
         'Development Status :: 4 - Beta',
